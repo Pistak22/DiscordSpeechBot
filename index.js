d731b79981c9fd5c86b098559ceee75426ed071a
Pistak22 <45733415+Pistak22@users.noreply.github.com>
2021-02-05 14:40:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -170,7 +170,7 @@ const DISCORD_MSG_LIMIT = 2000;
 const discordClient = new Discord.Client()
 discordClient.on('ready', () => {
     console.log(`Logged in as ${discordClient.user.tag}!`)
-    client.user.setActivity(`Használat: z!help`, { type: "LISTENING"});
+    discordClient.user.setActivity(`Használat: z!help`, { type: "LISTENING"});
 })
 discordClient.login(DISCORD_TOK)
 
